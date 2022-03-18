e8fdeded644a5c4728efc57c2a41c7bec0b89857
Agser25 <agustinsergic@gmail.com>
2022-03-18 20:32:06 -0300

    Finished HW 2

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -10,7 +10,6 @@ function obtenerMayor(x, y) {
   }
   return y;
 }
-
 function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
